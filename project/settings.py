39294b43caf4f3a74ce9806cc78ad7ba74c1b235
Lothar Tjipueja <lotharwebdev@gmail.com>
2024-09-27 15:30:19 +0200

    Allowed Hosts For Render

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-)_x)^)u0#*#uxmnzg8%pfac3vh%*n@3%0g^%a#_*!)m)h4m3#z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['binarycity-vrw7.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
