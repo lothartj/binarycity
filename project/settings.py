d7b3d476cf24fd1b516388591c119bbcd75ff0c3
Lothar Tjipueja <lotharwebdev@gmail.com>
2024-09-27 16:22:09 +0200

    dont event know what i changed

--- project/settings.py
+++ project/settings.py
@@ -25,6 +25,8 @@ SECRET_KEY = "django-insecure-)_x)^)u0#*#uxmnzg8%pfac3vh%*n@3%0g^%a#_*!)m)h4m3#z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+CSRF_TRUSTED_ORIGINS = ['https://binarycity-vrw7.onrender.com']
+
 ALLOWED_HOSTS = ['binarycity-vrw7.onrender.com', 'localhost', '127.0.0.1']
 
 
